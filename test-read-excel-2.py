9ecf262094d9d487a0b38157d87a44425a2597af
billtubbs <bill.tubbs@me.com>
2021-05-07 18:08:31 -0700

    Prints version info

--- test-read-excel-2.py
+++ test-read-excel-2.py
@@ -1,5 +1,9 @@
 import pandas as pd
+from platform import python_version
 import os
+
+print(f"Python: {python_version()}")
+print(f"Pandas: {pd.__version__}")
 
 data_dir = 'data'
 input_data_filename = 'excel_test2.xls'
